a8ea6f92a3ada07a2aab3ef13cca6c611b40b593
gautamdev17 <gautamd.nxt@gmail.com>
2026-03-09 19:28:26 +0530

    did some dumb mistakes

--- register_file.v
+++ register_file.v
@@ -1,3 +1,19 @@
-module register_file (input [4:0]readreg1,readreg2,writedata,input write_en,output [31:0]readout1,readout2);
-    
+module register_file #(parameter XLEN = 32) (input [4:0] readreg1,readreg2,writereg,input clk,write_en,input [31:0]write_data,
+output reg [31:0] readout1,readout2);
+    reg [XLEN-1:0] x [31:0]; // register memory
+    //reg [width-1:0] array_name [0:no.ofelements-1];
+    //caution:x0 mustbe always wired to zero. readin it is foine, writing is bad.
+
+    // combinational read
+    always @(*) begin
+        readout1 = x[readreg1]; //creates mux
+        readout2 = x[readreg2];
+        x[0] = 32'b0; //dumb
+    end
+
+    //sequential write
+    always @(posedge clk) begin
+        if(write_en && writereg!=5'b0)
+            x[writereg]<=write_data; 
+    end
 endmodule
