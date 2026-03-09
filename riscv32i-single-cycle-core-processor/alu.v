3fb9dd95fed0fcfb120a3974bb8e421fd9bafddf
gautamdev17 <gautamd.nxt@gmail.com>
2026-03-09 23:56:55 +0530

    60% alu is over, will kickstart tmrw :-)

--- riscv32i-single-cycle-core-processor/alu.v
+++ riscv32i-single-cycle-core-processor/alu.v
@@ -2,21 +2,27 @@ module alu #(parameter XLEN = 32) (input [XLEN-1:0]a,b,input [3:0]alu_sel,output
     always @(*) begin
         case (alu_sel)
             4'h0: c = a + b; //ADD
-            4'h1: c = b - a; //SUB
-            4'h2: logical = a < b; //SLT
-            4'h3: logical = a < //SLTU figure this out
+            4'h1: c = a - b; //SUB
+            4'h2: logical = a < b; //SLTU
+            4'h3:
+            begin
+                a = (a[XLEN-1]==1)?(~a + 32'd1):a;
+                b = (b[XLEN-1]==1)?(~b + 32'd1):b;
+                logical = a < b;
+            end //SLT figure this out
             4'h4: c = a & b;//AND
             4'h5: c = a | b;//OR
             4'h6: c = a ^ b;//XOR
             4'h7: c = a << b[4:0];//SLL
             4'h8: c = a >> b[4:0];//SRL
-            4'h9: c = {a[31],a >> b[4:0]};//SRA
+            4'h9: c = {a[XLEN-1],a >> b[4:0]};//SRA
             default: c = 32'b0;
         endcase
     end
 endmodule
 
 /*
+signed vs unsigned compare
 
 
 
