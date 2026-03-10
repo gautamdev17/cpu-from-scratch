b2332b99595db9e226250bae8bd7b392654b5f46
gautamdev17 <gautamd.nxt@gmail.com>
2026-03-10 14:33:47 +0530

    some changes

--- riscv32i-single-cycle-core-processor/alu.v
+++ riscv32i-single-cycle-core-processor/alu.v
@@ -1,17 +1,17 @@
-module alu #(parameter XLEN = 32) (input [XLEN-1:0]a,b,input [3:0]alu_sel,output reg [XLEN-1:0] c,output reg carry,logical);
+module alu #(parameter XLEN = 32) (input [XLEN-1:0]a,b,input [3:0]alu_sel,output reg [XLEN-1:0] c,output reg carry);
     always @(*) begin
-        c = 32'b0; logical = 0; carry = 0;
+        c = 32'b0;carry = 0;
         case (alu_sel)
-            4'h0: {carry,c} = a+b; //ADD
-            4'h1: c = a - b; //SUB
-            4'h2: logical = a < b; //SLTU
-            4'h3: logical = $signed(a) < $signed(b); //SLT figure this out, i wanted to do pure hw implementation
+            4'h0: {carry,c} = a + b; //ADD
+            4'h1: c = {31'b0,a-b}; //SUB
+            4'h2: c = {31'b0,a<b}; //SLTU
+            4'h3: c = $signed(a) < $signed(b); //SLT figure this out, i wanted to do pure hw implementation
             4'h4: c = a & b;//AND
             4'h5: c = a | b;//OR
             4'h6: c = a ^ b;//XOR
             4'h7: c = a << b[4:0];//SLL
             4'h8: c = a >> b[4:0];//SRL
-            4'h9: c = $signed(a) >> b[4:0];//SRA, preserving the sign of the number
+            4'h9: c = $signed(a) >>> b[4:0];//SRA, preserving the sign of the number
             // logical left = arithmetic left, but logical right ≠ arithmetic right
             // my idea: c = {b{a[XLEN-1]},(a >> b[4:0])[XLEN-1:b]}; ---> variable indexing and constant wont allow this
         endcase
