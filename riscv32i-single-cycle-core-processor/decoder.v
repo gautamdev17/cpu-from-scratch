d9b4578b8591046ff81377c7cf6d9af1c0bd4c2e
gautamdev17 <gautamd.nxt@gmail.com>
2026-03-10 16:55:15 +0530

    5pm push

--- riscv32i-single-cycle-core-processor/decoder.v
+++ riscv32i-single-cycle-core-processor/decoder.v
@@ -1,4 +1,10 @@
-module decoder (input [6:0]opcode,funct7,input [2:0]funct3,output control_signals);// should decide on output
+module decoder (input [31:0]inst,output control_signals);// should decide on output
+    wire [6:0]opcode;
+    assign opcode = inst[6:0];
+    wire [6:0]funct7;
+    assign funct7 = inst[31:25];
+    wire [2:0]funct3;
+    assign funct3 = inst[14:12];
     always @(*) begin
         case(opcode)
             7'b0110011: //r-type
@@ -15,9 +21,45 @@ module decoder (input [6:0]opcode,funct7,input [2:0]funct3,output control_signal
                     4'h6: //SLTU
                 endcase
             7'b0010011: //i-type
+                case(funct3)
+                    3'h0: //ADDI
+                    3'h4: //XORI
+                    3'h6: //ORI
+                    3'h7: //ANDI
+                    3'h1: //SLLI //ignoring imm[5:11] since there is no other case with this (opcode,funct3) comb
+                    3'h5: //SRLI & SRAI
+                          if(funct7[5]) // imm[5:11] found equivalent to imm[5:11]
+                            //SRLI
+                          else
+                            //SRAI
+                    3'h2: //SLTI
+                    3'h3: //SLTIU
+                endcase
             7'b0000011: //i-type (loads)
+                case(funct3)
+                    3'h0: //LB
+                    3'h1: //LH
+                    3'h2: //LW
+                    3'h4: //LBU
+                    3'h5: //LHU
+                endcase
             7'b0100011: //s-type
+                case(funct3)
+                    3'h0: //SB
+                    3'h1: //SH
+                    3'h2: //SW
+                endcase
             7'b1100011: //b-type
+                case(funct3)
+                    3'h0: //BEQ
+                    3'h1: //BNE
+                    3'h4: //BLT
+                    3'h5: //BGE
+                    3'h6: //BLTU
+                    3'h7: //BGEU
+                endcase
+            7'b1101111: // jal & jalr
+
         endcase
     end
 endmodule
