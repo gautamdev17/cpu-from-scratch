f0dc966edcb601c5be55b7b399fb08d7210b605e
gautamdev17 <gautamd.nxt@gmail.com>
2026-03-11 17:58:08 +0530

    revised pc design

--- riscv32i-single-cycle-core-processor/program_counter.v
+++ riscv32i-single-cycle-core-processor/program_counter.v
@@ -1,4 +1,4 @@
-module program_counter #(parameter XLEN = 32) (input [XLEN-1:0] pc_in,sign_extoffset,input clk,jump,rst,output reg [XLEN-1:0] pc_out);
+/*module program_counter #(parameter XLEN = 32) (input [XLEN-1:0] pc_in,sign_extoffset,input clk,jump,rst,output reg [XLEN-1:0] pc_out);
     always @(posedge clk) begin
         if (rst)
             pc_out<=32'b0;
@@ -6,5 +6,16 @@ module program_counter #(parameter XLEN = 32) (input [XLEN-1:0] pc_in,sign_extof
             pc_out<=pc_in+(sign_extoffset);
         else
             pc_out<=pc_in+32'd4;
+    end
+endmodule*/
+
+// revised edition:
+// reason: what i though was... the branching must be done on the top module. not inside the pc. pc is just a reg man.
+module program_counter #(parameter XLEN = 32) (input [XLEN-1:0] pc_in,input clk,rst,output reg [XLEN-1:0] pc_out);
+    always @(posedge clk) begin
+        if(rst)
+            pc_out<=32'b0;
+        else
+            pc_out<=pc_in;
     end
 endmodule
