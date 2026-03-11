78c0be8dea563f620174cb665645f3bfb0ff2d18
gautamdev17 <gautamd.nxt@gmail.com>
2026-03-11 23:32:03 +0530

    no need those updates

--- riscv32i-single-cycle-core-processor/alu.v
+++ riscv32i-single-cycle-core-processor/alu.v
@@ -12,12 +12,14 @@ module alu #(parameter XLEN = 32) (input [XLEN-1:0]a,b,input [3:0]alu_sel,output
             4'h7: c = a << b[4:0];//SLL
             4'h8: c = a >> b[4:0];//SRL
             4'h9: c = $signed(a) >>> b[4:0];//SRA, preserving the sign of the number
+            /*
             4'hA: c = {31'b0,(a==b)};//BEQ
             4'hB: c = {31'b0,(a!=b)};//BNE
             4'hC: c = {31'b0,($signed(a)<$signed(b))};//BLT
             4'hD: c = {31'b0,(($signed(a)>$signed(b)) | ($signed(a)==$signed(b)))};//BGE
             4'hE: c = {31'b0,(a<b)};//BLTU
             4'hF: c = {31'b0,((a>b) | (a==b))};//BGEU
+            */
             // logical left = arithmetic left, but logical right ≠ arithmetic right
             // my idea: c = {b{a[XLEN-1]},(a >> b[4:0])[XLEN-1:b]}; ---> variable indexing and constant wont allow this
         endcase
