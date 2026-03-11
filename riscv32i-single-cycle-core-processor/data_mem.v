db012fce4cc82229db25f09b37eb86c2a7fe283e
gautamdev17 <gautamd.nxt@gmail.com>
2026-03-11 16:20:01 +0530

    pushed dmem

--- riscv32i-single-cycle-core-processor/data_mem.v
+++ riscv32i-single-cycle-core-processor/data_mem.v
@@ -1,4 +1,4 @@
-module data_mem (input [31:0]addr,output reg [31:0]data);
+module data_mem (input [31:0]addr,wr_data,input wr_en,clk,output reg [31:0]data);
     reg [7:0] dmem [2**32-1 :0]; 
     // addressability is byte, addresses is 32-bit space, so 2^32 spaces available
     // 2^32 = 2^2GiB = 4 GiB of memory
@@ -6,6 +6,10 @@ module data_mem (input [31:0]addr,output reg [31:0]data);
     // so my output will be 32 bit sized. based on control signals(instruction),it can branch the sizes
     always @(*) begin
         data = {dmem[addr+3],dmem[addr+2],dmem[addr+1],dmem[addr]};
+    end
+    always @(posedge clk) begin
+        if(wr_en)
+        {dmem[addr+3],dmem[addr+2],dmem[addr+1],dmem[addr]} <= wr_data;
     end
 endmodule
 
